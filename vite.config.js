12ad2ff77e00d00b124353f8339eef9008d97335
Kris <krisnakumars0101@gmail.com>
2025-03-17 21:10:59 +0530

    404 for error bug fix 2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // Remove tailwindcss() from plugins
+  plugins: [tailwindcss(),react()],
   server: {
     historyApiFallback: true, // Fixes 404 on refresh
   },
-});
+})
